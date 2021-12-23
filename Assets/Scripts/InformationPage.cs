82ab8728e365de25247bb6c4b8833e5cd48878f4
Gefen <gefenbar009@gmail.com>
2021-12-23 14:50:17 +0200

    Dynamic bag inside profile scene

---
+++ Assets/Scripts/InformationPage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationPage : MonoBehaviour
+{
+    public GameObject apple;
+    public GameObject scroll;
+    public GameObject potion;
+
+    void Start()
+    {
+        if (Player.apple == 0)
+        {
+            apple.GetComponent<Image>().color = new Color32(150, 150, 150, 150);
+        }
+        if (Player.scroll == 0)
+        {
+            apple.GetComponent<Image>().color = new Color32(150, 150, 150, 150);
+        }
+        if (Player.potion == 0)
+        {
+            apple.GetComponent<Image>().color = new Color32(150, 150, 150, 150);
+
+        }
+    }
+}
