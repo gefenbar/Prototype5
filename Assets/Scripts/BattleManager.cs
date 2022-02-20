2766790e1a19d402f461cefb5ce022c7a3ad86ad
Gefen <gefenbar009@gmail.com>
2022-02-20 13:38:04 +0200

    fix

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -63,7 +63,7 @@ public class BattleManager : MonoBehaviour
         enabled = enemy;
         disabled = Player.Instance;
         disabledHealthBar = playerHealthBar;
-        if (distance < 2.5 && & enemy.attack >= Player.Instance.attack)
+        if (distance < 2.5 && enemy.attack >= Player.Instance.attack)
         {
             Attack();
         }
